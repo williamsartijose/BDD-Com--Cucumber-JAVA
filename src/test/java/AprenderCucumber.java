39e2ef463476aef1c535b1dbbcec0d7afa65fba4
unknown <williamsartijose@hotmail.com>
2023-01-18 22:09:18 +0000

    CucumberOptions

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -4,19 +4,20 @@ import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
 
+	
 	@Dado("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
-		
+	public void queCrieiOArquivoCorretamente() throws Throwable {
+	  
 	}
 
 	@Quando("^executá-lo$")
-	public void executá_lo() throws Throwable {
-
+	public void executáLo() throws Throwable {
+	  
 	}
 
 	@Então("^a especificação deve finalizar com sucesso$")
-	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-
-	}
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+	    
+	}	
 
 }
--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,8 +1,19 @@
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = "pretty",
+		monochrome = true,
+		snippets = SnippetType.CAMELCASE,
+		dryRun = false,
+		strict = false
+		
+		
+		)
 public class Runner {
 
 }
